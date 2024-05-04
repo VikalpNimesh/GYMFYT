26205e53e17c66ad09c59748b5fcaf4cc88aab38
vikalp <vikalpnimesh786@gmail.com>
2024-05-04 23:10:40 +0530

    mongo db connected

---
+++ server/src/app.js
@@ -0,0 +1,17 @@
+// import express from "express";
+// import dotenv from "dotenv"
+// const app = express();
+
+// // config.require("dotenv")
+
+// const PORT = process.env.PORT || 3000
+
+// app.get("/", (req, res) => {
+//   res.send("hi there kaise ho");
+// });
+
+// app.post("/", (req, res) => {});
+
+// app.listen(PORT, () => {
+//   console.log("Connected");
+// });
