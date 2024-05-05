08cb99f03889cd0669430c174a74334e32b1b69b
vikalp <vikalpnimesh786@gmail.com>
2024-05-05 11:43:09 +0530

    user model created

---
+++ server/src/models/user.model.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: [true, "username is required"],
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: [true, "fullname is required"],
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    phoneNumber: Number,
+    DOB: String,
+    profileImage: String,
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
